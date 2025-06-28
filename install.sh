952dd47fbf75431dafa1332a7c7801f48b8e80c4
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-28 09:00:20 +0200

    Allows "pack" argument to create zip
    Extends the install script to allow the "pack" argument,
    which also triggers the zip creation. This provides
    an alternative way to create the zip file without
    immediately attempting installation.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=\ui --extra-so
 cd ..
 mv messagingmenu@lauinger-clan.de.shell-extension.zip messagingmenu@lauinger-clan.de.zip
 
-if [ "$1" = "zip" ]; then
+if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
    echo "Extension zip created ..."
 else
    gnome-extensions install messagingmenu\@lauinger-clan.de.zip --force
